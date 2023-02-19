9e91249c05fb6f98db8a5ad0bb7a986ab0c41c50
manucarenjot <carenjotje@gmail.com>
2023-02-19 20:10:13 +0100

    installation + création d'un compenants

---
+++ vue3-ubereat-tuto/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <RestaurantCard/>
+
+  </div>
+</template>
+
+<script>
+import RestaurantCard from "@/components/RestaurantCard.vue";
+
+export default {
+  name: 'App',
+  components: {
+    RestaurantCard
+  }
+}
+</script>
+
+<style>
+@import url(style/reset.css);
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-ubereat-tuto/src/components/RestaurantCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="restaurant--card">
+    <div class="restaurant--image">
+    </div>
+    <div class="restaurant--informations">
+      <div class="top">
+        <p class="name">
+          subway
+        </p>
+        <p class="rating">
+          4.5
+        </p>
+        <p class="time">9 min
+        </p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "RestaurantCard.vue"
+}
+</script>
+
+<style scoped>
+.restaurant--card {
+  width: 32%;
+  height: 30vh;
+}
+
+.restaurant--image {
+  background-image: url(../assets/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==.webp);
+  width: 100%;
+  height: 70%;
+  background-size: cover;
+  background-position: center;
+}
+
+.restaurant--informations .top {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
